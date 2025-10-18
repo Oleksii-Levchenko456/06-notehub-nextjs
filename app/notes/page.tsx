b71cd38558d5ca3e437e4e388323d1e97432a2b0
Oleksii Levchenko <neonator3000@gmail.com>
2025-10-18 18:03:49 +0300

    noteHub next-js

---
+++ app/notes/page.tsx
@@ -0,0 +1,23 @@
+import NotesClient from "./Notes.client";
+import { QueryClient, HydrationBoundary, dehydrate } from "@tanstack/react-query"
+import { fetchNotes, } from "@/lib/api"
+
+
+export default async function Notes() {
+
+    const queryClient = new QueryClient()
+
+    await queryClient.prefetchQuery({
+        queryKey: ['notes', 1, ''],
+        queryFn: () => fetchNotes(1, 12, ''),
+    })
+
+
+    return (
+
+        <HydrationBoundary state={dehydrate(queryClient)}>
+            <NotesClient />
+        </HydrationBoundary>
+
+    )
+}
